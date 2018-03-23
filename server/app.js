4ca6f5294f27c95e0a0b0782f5cf0e2ad4690a19
Dylan Espinosa <espinosadylan17@gmail.com>
2018-03-23 09:44:08 -0700

    Ready for deployment

--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require("morgan");
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongooseUri = process.env.MONGODB_URI ? 'mongodb://heroku_5bll6n7g:uckcj5d9og8d5b9i60i03ghtfa@ds123259.mlab.com:23259/heroku_5bll6n7g'
+                : "mongodb://localhost/my-blog";
+
+mongoose.connect(mongooseUri);
 mongoose.Promise = Promise;
 
 const app = express();
