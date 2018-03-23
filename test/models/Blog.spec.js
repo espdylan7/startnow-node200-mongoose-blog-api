afbc2a3d6e1f7865e09ea72d83a2a92ff47293de
Dylan Espinosa <espinosadylan17@gmail.com>
2018-03-23 11:09:57 -0700

    Ready to deploy

--- test/models/Blog.spec.js
+++ test/models/Blog.spec.js
@@ -1,4 +1,4 @@
-const  expect  = require('chai');
+const  { expect }  = require('chai');
 const { ObjectId } = require('mongoose').Schema.Types;
 
 const { fakeBlogs, fakeUser } = require('../lib/fake');
